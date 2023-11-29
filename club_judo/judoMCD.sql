4af5d28f198b7c4f3dfd4caef09200452a3b8581
24ndoumbe <98036183+24ndoumbe@users.noreply.github.com>
2023-11-29 10:00:22 +0100

    Add files via upload

---
+++ club_judo/judoMCD.sql
@@ -0,0 +1,5 @@
+USERS: id, nom, prenom, date de naissance, age, poids, categorie, adresse mail, sexe, date_inscription, responsable 
+a, 11 USERS, 11 CREDENTIALS 
+CREDENTIALS: id, adresse mail, password 
+PRESENT, 0N CREDENTIALS, 0N COURS 
+COURS: id, date, heure_début, heure_fin, status
